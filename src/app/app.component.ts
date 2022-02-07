f6b36b40aa59dd123e023f45af81013a67d417e9
Alvin Christ Ardiansyah <alvinardiansyah2002@gmail.com>
2022-02-07 15:16:26 +0700

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 import { CentrifugeService } from './centrifuge.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy{
   title = 'test-centrifuge';
   counter = 0;
   counter2 = 0;
+  user = 0;
+  client = 0;
+  private _unsubAll: Subject<any>;
   constructor (
     public _cs: CentrifugeService
   ) {
     this._cs.init('ws://localhost:8000/connection/websocket','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM3MjIiLCJleHAiOjE2NDQ2NDQ3NTZ9.5G2UTUDQW6YJCGMnRuXEv9dhceWKypMGLoioTzAIx8I')
+    this._unsubAll = new Subject();
   }
 
   ngOnInit() {
-    this._cs.subscribe('ch1').on('publish', (ctx) => {
-      this.counter = ctx.data.value
+    this._cs.subscribe('ch1');
+    this._cs.subscribe('ch2');
+
+    this._cs.data['ch1'].pipe(takeUntil(this._unsubAll)).subscribe((data: any) => {
+      this.counter = data
     })
-    this._cs.subscribe('ch2').on('publish', (ctx) => {
-      this.counter2 = ctx.data.value
+    this._cs.data['ch2'].pipe(takeUntil(this._unsubAll)).subscribe((data: any) => {
+      this.counter2 = data
     })
+
     this._cs.connect();
-    setTimeout(() => {
-      this._cs.presenceStats('ch1')
-    },100)
+
+    this._cs.presenceStats_list['ch2'].pipe(takeUntil(this._unsubAll)).subscribe((data: any) => {
+      this.user = data.num_users
+      this.client = data.num_clients
+    })
+  }
+
+  ngOnDestroy(): void {
+    this._unsubAll.next();
+    this._unsubAll.complete();
   }
 }
--- src/app/centrifuge.service.ts
+++ src/app/centrifuge.service.ts
@@ -1,13 +1,39 @@
 import { Injectable } from '@angular/core';
 import Centrifuge from 'centrifuge';
+import { BehaviorSubject } from 'rxjs';
+
+interface PublishInterface {
+  data: any;
+  gen: any;
+  offset: any;
+  seq: any
+}
+
+interface JoinLeaveInterface {
+  info: {
+    client: string,
+    user: string
+  }
+}
+
+interface SubscribeInterface {
+  channel: string,
+  isResubscribe?: boolean,
+  recovered?: boolean
+}
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class CentrifugeService {
   private centrifuge!: Centrifuge;
   private subs: Centrifuge.Subscription[] = [];
+  public presenceStats_list: {[key: string]: BehaviorSubject<any>} = {} // kumpulan presenceStats dari channel" yang di subscribe ( "nama_channel": Stream )
+  public data: {[key: string]: BehaviorSubject<any>} = {}
   public status: any;
-  constructor() { }
+  constructor() {
+  }
 
   init(addr: string, token: string) {
     this.centrifuge = new Centrifuge(addr)
@@ -18,20 +44,55 @@ export class CentrifugeService {
     this.centrifuge.on('disconnect', (ctx) => {
       this.status = ctx
     })
-    this.centrifuge.on('message', (ctx) => {
-      console.log(ctx)
-    })
+  }
+
+  handlePublish(channel: string, ctx: PublishInterface): void {
+    console.log(ctx)
+    this.data[channel].next(ctx.data)
+  }
+
+  handleJoin(channel: string, msg: JoinLeaveInterface): void {
+    this.presenceStats(channel)
+    console.log(msg)
+  }
+
+  handleLeave(channel: string, msg: JoinLeaveInterface): void {
+    this.presenceStats(channel)
+    console.log(msg)
+  }
+
+  handleSubscribe(ctx: SubscribeInterface): void {
+    console.log(ctx)
+  }
+
+  handleUnsubscribe(ctx: SubscribeInterface): void {
+    console.log(ctx)
+  }
+
+  handleError(err: any): void {
+    console.log(err)
   }
 
   subscribe(channel: string) {
     const _c = this.subs.find(sub => sub.channel === channel) //cek apakah channel sudah di subscribe sebelumnya
     if(!_c){ //jika belum maka tambahkan ke dalam list
-      const c = this.centrifuge.subscribe(channel);
-      this.subs.push(c);
+      this.presenceStats_list[channel] = new BehaviorSubject({}) // tambah channel ke dalam stream presenceStat list
+      this.data[channel] = new BehaviorSubject({}) // tambah channel ke dalam stream data
+      const callbacks = {
+        "join": (ctx: any) => this.handleJoin(channel,ctx),
+        "leave": (ctx: any) => this.handleLeave(channel,ctx),
+        "publish": (ctx: any) => this.handlePublish(channel,ctx),
+        "subscribe": (ctx: any) => this.handleSubscribe(ctx),
+        "unsubscribe": (ctx: any) => this.handleUnsubscribe(ctx),
+        "error": (err: any) => this.handleError(err)
+      }
+      const c = this.centrifuge.subscribe(channel,callbacks)
+      this.subs.push(c); // tambah channel ke dalam subs list
       return c;
     }
     return _c; //jika sudah ada maka pakai yang sudah ada
   }
+
 
   connect(): any {
     this.centrifuge.connect();
@@ -46,12 +107,20 @@ export class CentrifugeService {
     if(idx !== -1){
       this.subs[idx].unsubscribe();
       this.subs[idx].removeAllListeners();
-      this.subs.splice(idx,1)
+      this.subs.splice(idx,1) // hapus channel dari sub list
+
+      this.presenceStats_list[channel].complete() // selesaikan stream
+      delete this.presenceStats_list[channel] // hapus channel dari stream presenceStats
+
+      this.data[channel].complete() // selesaikan stream
+      delete this.data[channel] // hapus channel dari stream data
+
       return true;
     }
     return false;
   }
 
+  //publish ke channel lain
   publish(channel: string, data: any): void { //kalau "publish": true di config.js
     this.centrifuge.publish(channel,data).catch((err) => {
       console.log(err)
@@ -67,8 +136,8 @@ export class CentrifugeService {
   presenceStats(channel: string): void {
     const c = this.subs.find(sub => sub.channel === channel)
     if(c) this.centrifuge.presenceStats(channel).then((res) => {
-      console.log(res)
-    }).catch((err) => {
+      this.presenceStats_list[channel].next(res)
+    }).catch((err: {message: string, code: number}) => {
       console.log(err)
     })
   }
